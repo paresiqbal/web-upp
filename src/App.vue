6559161a694a0dc98f75e42fe403dd58fc90845a
paresiqbal <thecrowdel@gmail.com>
2023-08-19 22:18:53 +0700

    Change to light mode

--- src/App.vue
+++ src/App.vue
@@ -4,13 +4,6 @@ import Navbar from "./components/Navbar.vue";
 
 <template>
   <Navbar />
-  <div>
-    <h1 class="title">Maybe i'm not using talwind</h1>
-  </div>
 </template>
 
-<style scoped>
-.title {
-  font-size: 20px;
-}
-</style>
+<style scoped></style>
