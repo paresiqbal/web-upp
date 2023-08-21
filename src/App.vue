04b3eb8bbdb3367b9e98d3d3c31817be19d6498b
paresiqbal <thecrowdel@gmail.com>
2023-08-21 19:16:17 +0700

    Add footer

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,14 @@
 <script setup>
 import Navbar from "./components/Navbar.vue";
 import Topbar from "./components/Topbar.vue";
-import Home from "./views/Home.vue";
+import Footer from "./components/Footer.vue";
 </script>
 
 <template>
   <Topbar />
   <Navbar />
   <router-view></router-view>
+  <Footer />
 </template>
 
 <style scoped></style>
