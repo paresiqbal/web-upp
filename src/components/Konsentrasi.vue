a8ac8f80a2f536d69d39229e06bc26bf4a30e8d9
paresiqbal <thecrowdel@gmail.com>
2023-08-21 08:32:07 +0700

    Adding konsentrasi

---
+++ src/components/Konsentrasi.vue
@@ -0,0 +1,63 @@
+<script setup></script>
+
+<template>
+  <div class="flex flex-col items-center mx-20">
+    <h1 class="text-center text-4xl font-bold text-amber-500 py-10 underline">
+      Konsentrasi
+    </h1>
+    <div class="flex gap-4">
+      <div
+        class="card card-side bg-base-100 shadow-xl flex justify-center items-center px-4 rounded-md max-w-2xl hover:outline hover:outline-amber-500"
+      >
+        <figure>
+          <img
+            src="../assets/konsentrasi/coding.png"
+            alt="Movie"
+            class="w-20"
+          />
+        </figure>
+        <div class="card-body max-w-md">
+          <h2 class="card-title">Pemrograman</h2>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quasi, qui
+            eaque possimus velit eius laborum deleniti illum totam corporis
+            porro neque esse optio illo dolorem.
+          </p>
+          <div class="card-actions justify-end">
+            <button
+              class="py-2 px-4 bg-amber-500 font-bold text-white rounded-md"
+            >
+              Pelajari
+            </button>
+          </div>
+        </div>
+      </div>
+      <div
+        class="card card-side bg-base-100 shadow-xl flex justify-center items-center px-4 rounded-md max-w-2xl hover:outline hover:outline-amber-500"
+      >
+        <figure>
+          <img
+            src="../assets/konsentrasi/internet.png"
+            alt="Movie"
+            class="w-20"
+          />
+        </figure>
+        <div class="card-body max-w-md">
+          <h2 class="card-title">Jaringan</h2>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quasi, qui
+            eaque possimus velit eius laborum deleniti illum totam corporis
+            porro neque esse optio illo dolorem.
+          </p>
+          <div class="card-actions justify-end">
+            <button
+              class="py-2 px-4 bg-amber-500 font-bold text-white rounded-md"
+            >
+              Pelajari
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,12 +3,14 @@
 import Carousel from "../components/Carousel.vue";
 import Pmb from "../components/Pmb.vue";
 import Berita from "../components/Berita.vue";
+import Konsentrasi from "../components/Konsentrasi.vue";
 </script>
 
 <template>
   <Carousel />
   <Pmb />
   <Berita />
+  <Konsentrasi />
 </template>
 
 <style scoped></style>
