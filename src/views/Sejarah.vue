0a888cab11227aab0b12218fa3c8b175184c0c08
paresiqbal <thecrowdel@gmail.com>
2023-08-21 19:22:27 +0700

    Sejarah

--- src/views/Sejarah.vue
+++ src/views/Sejarah.vue
@@ -1,5 +1,33 @@
 <script setup></script>
 
 <template>
-  <h1>Sejarah</h1>
+  <div class="mx-20 py-10">
+    <h1 class="text-5xl font-bold text-amber-500 underline text-center pb-8">
+      Sejarah
+    </h1>
+    <div class="flex gap-2">
+      <div class="w-3 bg-amber-500"></div>
+      <div class="bg-slate-50 flex flex-col gap-2 text-lg">
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iure
+          excepturi illo veritatis est voluptate voluptatem in voluptatum,
+          consequatur voluptates aspernatur, dicta alias vel velit quisquam
+          tempora nesciunt. Quibusdam animi labore adipisci eveniet voluptatum,
+          obcaecati dolore modi perspiciatis voluptates, necessitatibus rem odio
+          aut, omnis et non? Explicabo quia tenetur numquam suscipit expedita
+          magnam blanditiis fugiat reiciendis atque fuga, voluptate ipsa nobis
+          earum ratione illo, officiis perferendis nemo voluptatem possimus
+          labore rem.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo
+          voluptates id maiores ad necessitatibus nulla! Ex corrupti accusantium
+          dicta minima vero accusamus maxime reiciendis voluptatem qui harum
+          ipsam, amet, corporis tempore molestiae. Est impedit velit error vel
+          at deleniti rerum exercitationem. Quos nam dolorum cumque accusantium
+          similique molestiae, harum rem?
+        </p>
+      </div>
+    </div>
+  </div>
 </template>
