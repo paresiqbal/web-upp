87e34a9f2f71cc8cdd36ee5734697da9e0ad04c7
paresiqbal <thecrowdel@gmail.com>
2023-08-22 22:19:43 +0700

    Add jadwal

--- src/views/Jadwal.vue
+++ src/views/Jadwal.vue
@@ -5,5 +5,13 @@
     <h1 class="text-5xl font-bold text-amber-500 underline text-center pb-8">
       Jadwal Kuliah
     </h1>
+    <div>
+      <h2 class="text-xl font-bold py-2">Semtester 1</h2>
+      <img src="../assets/image/" alt="jadwal1.png" />
+    </div>
+    <div>
+      <h2 class="text-xl font-bold py-2">Semtester 2</h2>
+      <img src="../assets/image/" alt="jadwal1.png" />
+    </div>
   </div>
 </template>
