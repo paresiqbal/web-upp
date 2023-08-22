d3b4e8cb92639d8f62f08ccb224a3f5eade8f820
paresiqbal <thecrowdel@gmail.com>
2023-08-22 22:14:46 +0700

    modified Beasiswa

--- src/views/Beasiswa.vue
+++ src/views/Beasiswa.vue
@@ -5,5 +5,29 @@
     <h1 class="text-5xl font-bold text-amber-500 underline text-center pb-8">
       Beasiswa
     </h1>
+    <div>
+      <h2 class="font-bold text-xl py-2">1. Beasiswa 1</h2>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Harum veniam
+        pariatur minima dolorum explicabo optio blanditiis doloremque in
+        accusamus sapiente ipsum porro quasi veritatis nemo minus quibusdam,
+        commodi nesciunt quos nobis distinctio fugit excepturi quidem maiores?
+        Iure veniam quos odit.
+      </p>
+    </div>
+    <div>
+      <h2 class="font-bold text-xl py-2">2. Beasiswa 2</h2>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Explicabo
+        saepe odio, placeat qui quibusdam repudiandae. Aperiam voluptas quam
+        vero! Neque natus quam blanditiis ratione facere reprehenderit quo eius
+        culpa repudiandae nisi ipsam aliquam, reiciendis totam, omnis hic, porro
+        temporibus corporis optio veniam earum doloribus dolorem ipsum.
+        Praesentium incidunt laudantium exercitationem?
+      </p>
+      <img src="../assets/image/template.jpg" alt="template" class="w-full" />
+      <h3 class="font-bold text-xl py-2">Alur Beasiswa</h3>
+      <img src="../assets/image/template.jpg" alt="template" class="w-full" />
+    </div>
   </div>
 </template>
