6291ad5689ca21ba7c0bd5ab975613ba8fc6e6d1
paresiqbal <thecrowdel@gmail.com>
2023-08-22 22:15:36 +0700

    add rps

--- src/views/Rps.vue
+++ src/views/Rps.vue
@@ -5,5 +5,6 @@
     <h1 class="text-5xl font-bold text-amber-500 underline text-center pb-8">
       RPS
     </h1>
+    <img src="../assets/image/rps.png" alt="rps" class="w-full" />
   </div>
 </template>
