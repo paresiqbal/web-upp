de47bc80aaf47248afcb9f103ceb830cd2452dcc
paresiqbal <thecrowdel@gmail.com>
2023-08-22 22:34:55 +0700

    modified Kontak

--- src/views/Kontak.vue
+++ src/views/Kontak.vue
@@ -1,9 +1,15 @@
 <script setup></script>
 
 <template>
-  <div class="mx-20 py-10">
+  <div class="mx-20 py-10 flex flex-col justify-center items-center gap-10">
     <h1 class="text-5xl font-bold text-amber-500 underline text-center pb-8">
       Kontak
     </h1>
+    <img src="../assets/image/map.png" alt="" />
+    <div class="text-center flex gap-10 text-xl font-bold justify-between">
+      <h2>Alamat</h2>
+      <h2>No Telp</h2>
+      <h2>Email</h2>
+    </div>
   </div>
 </template>
