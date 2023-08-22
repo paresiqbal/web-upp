e6eee0f080f06d60ec1a81dd27e49bc27879acb9
paresiqbal <thecrowdel@gmail.com>
2023-08-22 22:21:45 +0700

    modified jumblah Mahasiswa

--- src/views/Mahasiswa.vue
+++ src/views/Mahasiswa.vue
@@ -5,5 +5,17 @@
     <h1 class="text-5xl font-bold text-amber-500 underline text-center pb-8">
       Mahasiswa
     </h1>
+    <div>
+      <h2 class="text-xl font-bold py-2">Semster 1</h2>
+      <img src="../assets/image/c1.png" alt="" />
+    </div>
+    <div>
+      <h2 class="text-xl font-bold py-2">Semster 2</h2>
+      <img src="../assets/image/c2.png" alt="" />
+    </div>
+    <div>
+      <h2 class="text-xl font-bold py-2">Semster 3</h2>
+      <img src="../assets/image/c3.png" alt="" />
+    </div>
   </div>
 </template>
