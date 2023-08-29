58d4931c63dbbaba81af4deb3f8427ef2553cab1
paresiqbal <thecrowdel@gmail.com>
2023-08-29 20:17:14 +0700

    Fixing Carousel

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -1,65 +1,46 @@
 <script>
-import { register } from "swiper/element/bundle";
+// import { register } from "swiper/element/bundle";
 
-register();
+// register();
 
-export default {
-  setup() {
-    const spaceBetween = 10;
-    const onProgress = (e) => {
-      const [swiper, progress] = e.detail;
-      console.log(progress);
-    };
+// export default {
+//   setup() {
+//     const spaceBetween = 10;
+//     const onProgress = (e) => {
+//       const [swiper, progress] = e.detail;
+//       console.log(progress);
+//     };
 
-    const onSlideChange = (e) => {
-      console.log("slide changed");
-    };
+//     const onSlideChange = (e) => {
+//       console.log("slide changed");
+//     };
 
-    return {
-      spaceBetween,
-      onProgress,
-      onSlideChange,
-    };
-  },
-};
+//     return {
+//       spaceBetween,
+//       onProgress,
+//       onSlideChange,
+//     };
+//   },
+// };
 </script>
 
 <template>
-  <div>
-    <swiper-container
-      :slides-per-view="1"
-      :space-between="spaceBetween"
-      :centered-slides="true"
-      :pagination="{
-        hideOnClick: true,
-      }"
-      :breakpoints="{
-        768: {
-          slidesPerView: 1,
-        },
-      }"
-      @progress="onProgress"
-      @slidechange="onSlideChange"
-    >
-      <swiper-slide class="swiper"
-        ><img
-          src="../assets/carousel/image1.jpg"
-          alt="carousel"
-          class="carousel-img"
-      /></swiper-slide>
-      <swiper-slide
-        ><img
-          src="../assets/carousel/image2.jpg"
-          alt="carousel"
-          class="carousel-img"
-      /></swiper-slide>
-      <swiper-slide
-        ><img
-          src="../assets/carousel/image3.jpg"
-          alt="carousel"
-          class="carousel-img"
-      /></swiper-slide>
-    </swiper-container>
+  <div class="carousel w-full">
+    <div id="item1" class="carousel-item w-full">
+      <img src="../assets/carousel/image1.jpg" class="w-full h-screen" />
+    </div>
+    <div id="item2" class="carousel-item w-full">
+      <img src="../assets/carousel/image2.jpg" class="w-full h-screen" />
+    </div>
+    <div id="item3" class="carousel-item w-full">
+      <img src="../assets/carousel/image3.jpg" class="w-full h-screen" />
+    </div>
+  </div>
+  <div class="flex justify-center w-full py-2 gap-2">
+    <a href="#item1" class="btn btn-xs">1</a>
+    <a href="#item2" class="btn btn-xs">2</a>
+    <a href="#item3" class="btn btn-xs">3</a>
+    <a href="#item4" class="btn btn-xs">4</a>
   </div>
 </template>
 
